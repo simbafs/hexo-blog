2f893cfe6bee64e79a7ee2e25bbb0f91c08ebc5c
simba-fs <simba.fs@gmail.com>
2020-08-20 22:54:55 +0800

    script/recorverDate.sh

--- script/recorverDate.sh
+++ script/recorverDate.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-for i in $(cat t);do 
-	n=$(echo $i | cut -d: -f1); 
-	d=$(echo $i | cut -d: -f2); 
-	touch -t$d $n; 
+dir=source/_posts
+
+grep -rnw $dir -e 'date' > t
+cut -d: -f1 t > name
+cut -d' ' -f2,3 t | sed 's/[- ]//g' | sed 's/://' | sed 's/:/./' > time
+paste -d: name time > t
+
+for i in $(cat t);do
+	n=$(echo $i | cut -d: -f1);
+	d=$(echo $i | cut -d: -f2);
+	touch -t$d $n;
 done
+
+rm t name time
